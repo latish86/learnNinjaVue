b9312a612396f712fe5440b902d0203db5d5fa36
latish86 <latish86@bk.ru>
2017-11-25 12:52:36 +0700

    Данные и  методы

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 new Vue({
   el: '#vue-app',
-  data:{
-    name: {
-      name: 'Tutor VUe'
+  data: {
+    name: 'Artem',
+    job: 'Ninjas' 
+  },
+  methods:{
+    good: function(time){
+      return 'Good ' + time + ' ' + this.name + '!!!'
     }
   }
 });
