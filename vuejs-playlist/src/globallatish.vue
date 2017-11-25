e3b195d23a08c8e1070e8053d7137163ab2ee922
latish86 <latish86@bk.ru>
2017-11-25 21:55:23 +0700

    Локальная и глобальная регистрация компонентов

---
+++ vuejs-playlist/src/globallatish.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <div><h1>Глобальный компонент</h1></div>
+    <li v-for="ninja in ninjas">{{ ninja }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['Yoishi', 'MArio', 'Mira', 'Koko']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import La  from './globallatish.vue'
+
+Vue.component('ninjas', La)
 
 new Vue({
   el: '#app',
