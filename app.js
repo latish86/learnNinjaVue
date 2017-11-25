47d1619becbdecfffda30fa86209567f5154ce6c
latish86 <latish86@bk.ru>
2017-11-25 16:54:37 +0700

    Динамический CSS с помощью классов

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: '',
-    job: 'Ninjas', 
-    website: 'http://ijun.ru',
-    websiteTag : '<a href="http://ijun.ru">Site</a>',
-    age: 20,
-    a: 0,
-    b: 0
+    red: false,
+    ital: true,
+    blue: true
   },
   methods:{
-    // addToA: function(){
-    //   console.log('testAAAA')
-    //   return this.a + this.age
-    // },
-    // addToB: function(){
-    //   console.log('testBBB')
-    //   return this.b + this.age
-    // },
-    // test: function(){
-    //   console.log('test')
-    // }
+    checkColor: function(){
+      this.red = !this.red;
+    },
+    checkStyle: function(){
+      this.ital = !this.ital;
+    }
   },
   computed:{
-    addToA: function(){
-      console.log('testAAAA')
-      return this.a + this.age
-    },
-    addToB: function(){
-      console.log('testBBB')
-      return this.b + this.age
+    Ex2Classes: function(){
+      return {
+        blue: this.blue,
+        ital: this.ital
+      }
     }
   }
 });
