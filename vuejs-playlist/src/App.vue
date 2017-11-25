a2663a4eb334ef559f526fa402dfdddf6be22755
latish86 <latish86@bk.ru>
2017-11-26 00:08:08 +0700

    Работа с Props. Валидация Props.

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
-    <app-content></app-content>
+    <app-content v-bind:ninjas="ninjas"></app-content>
     <app-footer></app-footer>
   </div>
 </template>
@@ -20,6 +20,14 @@ export default {
   },
   data () {
     return {
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
     }
   }
 }
