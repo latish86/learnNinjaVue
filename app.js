a1f3fe9ad29f2b54c3b5137b92cb57d96bd6efa8
latish86 <latish86@bk.ru>
2017-11-25 13:53:43 +0700

    v-bind, v-html. Вставка данных в атрибуты тегов. ВСтавка целых элементов HTML.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Artem',
-    job: 'Ninjas' 
+    job: 'Ninjas', 
+    website: 'http://ijun.ru',
+    websiteTag : '<a href="http://ijun.ru">Site</a>'
   },
   methods:{
     good: function(time){
